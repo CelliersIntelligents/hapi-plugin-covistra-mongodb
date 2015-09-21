f326ae6e1a2a47ef4d87b5ee198514c66182bb2d
Joel Grenon <joelgrenon@covistra.com>
2015-09-21 13:47:32 -0400

    Remove dependency management from Hapi and use internal Cmbf

--- index.js
+++ index.js
@@ -22,89 +22,84 @@ var P = require('bluebird'),
 
 var MongoClient = P.promisifyAll(mongodb.MongoClient);
 
+exports.deps = ['covistra-system'];
+
 exports.register = function (server, options, next) {
 
-    server.dependency(['covistra-system'], function(plugin, done) {
+    // Retrieve a reference to the current system configuration
+    var config = server.plugins['hapi-config'].CurrentConfiguration;
+    var systemLog = server.plugins['covistra-system'].systemLog;
 
-        // Retrieve a reference to the current system configuration
-        var config = server.plugins['hapi-config'].CurrentConfiguration;
-        var systemLog = server.plugins['covistra-system'].systemLog;
+    systemLog.info("Registering the MongoDB plugin");
+    systemLog.debug("Registering schema manager");
 
-        systemLog.info("Registering the MongoDB plugin");
-        systemLog.debug("Registering schema manager");
+    // Expose schema manager
+    var schemaManager = require('./lib/schema-manager')(server, systemLog.child({service: 'schema-manager'}));
+    server.expose('schemaManager', schemaManager);
 
-        // Expose schema manager
-        var schemaManager = require('./lib/schema-manager')(server, systemLog.child({service: 'schema-manager'}));
-        server.expose('schemaManager', schemaManager);
+    // Expose index manager
+    var indexManager = require('./lib/index-manager')(server, systemLog.child({service: 'index-manager'}));
+    server.expose('indexManager', indexManager);
 
-        // Expose index manager
-        var indexManager = require('./lib/index-manager')(server, systemLog.child({service: 'index-manager'}));
-        server.expose('indexManager', indexManager);
+    // Expose a few helpers
+    server.expose('uniqueCheck', require('./lib/unique-check')(server, systemLog));
+    server.expose('ObjectId', mongodb.ObjectId);
 
-        // Expose a few helpers
-        server.expose('uniqueCheck', require('./lib/unique-check')(server, systemLog));
-        server.expose('ObjectId', mongodb.ObjectId);
+    //TODO: Generic DBRef resolver (with field selection, promise-based)
 
-        //TODO: Generic DBRef resolver (with field selection, promise-based)
+    var DataSeeder = require('./lib/data-seeder')(server, systemLog, config);
 
-        var DataSeeder = require('./lib/data-seeder')(server, systemLog, config);
+    systemLog.debug( "Connecting to all configured MongoDB databases");
 
-        systemLog.debug( "Connecting to all configured MongoDB databases");
-
-        // Looping through all configured DB instances
-        return P.map(config.get('plugins:mongodb:dbs') || [], function(dbs) {
-            systemLog.debug("Configuring database %s (%s)", dbs.name, dbs.uri);
-            return MongoClient.connectAsync(dbs.uri, _.defaults(dbs.options || {}, {safe:true, db: { slaveOk: true}})).then(function(db) {
-                server.expose(dbs.name, db);
-                return {
-                    name: dbs.name,
-                    uri: dbs.uri,
-                    db: db
-                };
-            });
-
-        }).then(function(dbs) {
-            server.expose('dbs', dbs);
-            systemLog.info("%d database(s) have been connected", dbs.length);
-            return P.map(dbs, function(db) {
-                return indexManager.ensureIndexes(db.db, db.name);
-            }).then(function() {
-                systemLog.info("All indexes were created or updated in all databases");
-                if(options.testMode) {
-                    return require('./test/setup-test-mode')(server, systemLog, config, dbs, options);
-                }
-                else {
-
-                    // Perform data seeding
-                    var seedingCfg = config.get('plugins:mongodb:db-seeding');
-                    var plugins = config.get('plugins');
-
-                    return P.map(_.keys(plugins), function(pluginName) {
-                        var plugin = plugins[pluginName];
-                        if(plugin && plugin['seed-data']) {
-                            systemLog.debug("Checking to seed data for plugin %s", pluginName);
-                            return P.each(_.keys(plugin['seed-data']), function(dbName) {
-                                systemLog.debug("Initiating seeding for database %s", dbName);
-                                var ds = new DataSeeder(dbName, plugin['seed-data'][dbName], seedingCfg);
-                                return ds.seed();
-                            });
-                        }
-                    });
-                }
-            }).then(function() {
-                systemLog.info("All databases were successfully complete");
-                done();
-            });
-
-        }).catch(function (err) {
-            systemLog.error(err);
-            done(err);
+    // Looping through all configured DB instances
+    return P.map(config.get('plugins:mongodb:dbs') || [], function(dbs) {
+        systemLog.debug("Configuring database %s (%s)", dbs.name, dbs.uri);
+        return MongoClient.connectAsync(dbs.uri, _.defaults(dbs.options || {}, {safe:true, db: { slaveOk: true}})).then(function(db) {
+            server.expose(dbs.name, db);
+            return {
+                name: dbs.name,
+                uri: dbs.uri,
+                db: db
+            };
         });
 
+    }).then(function(dbs) {
+        server.expose('dbs', dbs);
+        systemLog.info("%d database(s) have been connected", dbs.length);
+        return P.map(dbs, function(db) {
+            return indexManager.ensureIndexes(db.db, db.name);
+        }).then(function() {
+            systemLog.info("All indexes were created or updated in all databases");
+            if(options.testMode) {
+                return require('./test/setup-test-mode')(server, systemLog, config, dbs, options);
+            }
+            else {
+
+                // Perform data seeding
+                var seedingCfg = config.get('plugins:mongodb:db-seeding');
+                var plugins = config.get('plugins');
+
+                return P.map(_.keys(plugins), function(pluginName) {
+                    var plugin = plugins[pluginName];
+                    if(plugin && plugin['seed-data']) {
+                        systemLog.debug("Checking to seed data for plugin %s", pluginName);
+                        return P.each(_.keys(plugin['seed-data']), function(dbName) {
+                            systemLog.debug("Initiating seeding for database %s", dbName);
+                            var ds = new DataSeeder(dbName, plugin['seed-data'][dbName], seedingCfg);
+                            return ds.seed();
+                        });
+                    }
+                });
+            }
+        }).then(function() {
+            systemLog.info("All databases were successfully complete");
+            next();
+        });
+
+    }).catch(function (err) {
+        systemLog.error(err);
+        next(err);
     });
-
-    next();
-
 
 };
 
